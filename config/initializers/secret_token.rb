dbf7889b28406af3c4ba1e4bdc6db74e74ed0a87
Andrey Seleznov <andrewaka39@gmail.com>
2014-03-24 11:53:49 +0200

    1 Инит

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notifyme::Application.config.secret_key_base = '332c3604aa28ac445b004ca6f338571deebf2ecb2ff613ad249887f869212977a97fa88a5f4673f828941d4ac17ce9e051efc64736c49a40280bf25d757adc85'
