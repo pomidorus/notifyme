78fc03f3615f80e0fdb0f363f1e7fc88fe3416ea
Andrey Seleznov <andrewaka39@gmail.com>
2014-03-25 13:19:37 +0200

    2 Авторизация

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,5 @@
 module MainHelper
+  def show_tickets(url)
+    content_tag(:div,'', id: 'tickets', data: {url: url})
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
 Ticket.delete_all
+
+User.delete_all
+User.create!({username: 'registrator', email: 'ww@gmail.com', password: 'testtest'})
